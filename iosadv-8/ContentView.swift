9d21d346d62a3420223ba05c5a0bb9f60cb9ae09
Dmitrii Lobanov <oftkor@gmail.com>
2022-06-16 22:51:35 +0300

    Initial Commit

---
+++ iosadv-8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosadv-8
+//
+//  Created by Dmitrii Lobanov on 16.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosadv-8/iosadv_8App.swift
@@ -0,0 +1,17 @@
+//
+//  iosadv_8App.swift
+//  iosadv-8
+//
+//  Created by Dmitrii Lobanov on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct iosadv_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
